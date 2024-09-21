3db0e664d1c1c5a980bd49b6182a952cf9940a7b
hunain12iqbal <hunnayyt@gmail.com>
2024-09-21 19:59:07 +0500

    odoo APP

---
+++ take_off_custom/__manifest__.py
@@ -0,0 +1,18 @@
+{
+
+    'name': 'Take Of Customization APP',
+    'version': '14.0.1.1.0',
+    'category': '',
+    'summary': """""",
+    'author': 'Hunain',
+    'website': "",
+    'depends': ['base', 'product'],
+    'data': [
+        'data/data.xml'
+    ],
+    
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
